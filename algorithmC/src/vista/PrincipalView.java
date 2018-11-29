aa888d99172eae15997f730dcf610b86035196b0
JuanJose <juajoflo@espol.edu.ec>
2018-11-28 19:27:02 -0500

    Cambios

--- algorithmC/src/vista/PrincipalView.java
+++ algorithmC/src/vista/PrincipalView.java
@@ -257,25 +257,26 @@ public class PrincipalView {
     private void botonComparar() {
         comparar.setOnAction(e -> {
             int valorAnalizar = this.validarNumero(spinner.getEditor().getText());
-            int numLines = OperationFile.cantidadElementos(ruta);
-            int numLinesEnteros = OperationFile.cantidadLineasEnteros(ruta);
-            System.out.println("C1: " + numLines);
-            System.out.println("C2: " + numLinesEnteros);
+            //int numLines = OperationFile.cantidadElementos(ruta);
+            //int numLinesEnteros = OperationFile.cantidadLineasEnteros(ruta);
+           // System.out.println("C1: " + numLines);
+            //System.out.println("C2: " + numLinesEnteros);
             if (ruta == null) {
                 DialogWindow.dialogoAdvertenciaArchivo();
             } else if (valorAnalizar == -1) {
                 DialogWindow.dialogoAdvertenciaNumeros();
             } else if (notChecked()) {
                 DialogWindow.dialogoAdvertenciaCheckBox();
-            } else if (valorAnalizar < 20) {
-                DialogWindow.dialogoAdvertenciaDatos();}
+            } 
+//            else if (valorAnalizar < 20) {
+//                DialogWindow.dialogoAdvertenciaDatos();}
             //} else if (numLines == numLinesEnteros) {
             else{
 //                List<Integer> arraylist = OperationFile.loadData(ruta, valorAnalizar);
 //                Sort prueba = new Sort(arraylist, this.merge.isSelected(), this.quick.isSelected(), this.insert.isSelected(), this.stooge.isSelected());
 //                prueba.allSort();
 //                this.Graficar(prueba);
-                    this.comparar.setDisable(true);
+//                    this.comparar.setDisable(true);
                progressBar.setProgress(0);
                 
                 List<Integer> arraylist = OperationFile.loadData(ruta,valorAnalizar);
